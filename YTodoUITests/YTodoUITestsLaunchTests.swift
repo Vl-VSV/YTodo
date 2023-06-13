cc2313209d04ecd06aaf176034baf1b64a3ee588
Vl-VSV <vvs_2015@icloud.com>
2023-06-13 21:13:04 +0300

    Initial Commit

---
+++ YTodoUITests/YTodoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  YTodoUITestsLaunchTests.swift
+//  YTodoUITests
+//
+//  Created by Vlad V on 13.06.2023.
+//
+
+import XCTest
+
+final class YTodoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
