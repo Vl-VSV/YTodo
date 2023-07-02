5efaa9bf470fa45ddcc0d7840627a35a571e30e3
Vl-VSV <vvs_2015@icloud.com>
2023-07-02 18:28:18 +0300

    Remove pods

--- Pods/Target Support Files/Pods-YTodo-YTodoUITests/Pods-YTodo-YTodoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YTodo_YTodoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_YTodo_YTodoUITests
-@end
--- Pods/Target Support Files/Pods-YTodo/Pods-YTodo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YTodo : NSObject
-@end
-@implementation PodsDummy_Pods_YTodo
-@end
