2b2af3e580b7c41f3b7524e3f1b2bc4f342550f4
Vl-VSV <vvs_2015@icloud.com>
2023-07-11 13:43:23 +0300

    Added functionality for saving and uploading data to SQLite database

--- YTodo/Models/FileCache.swift
+++ YTodo/Models/FileCache.swift
@@ -6,11 +6,13 @@
 //
 
 import Foundation
+import SQLite
 
 // MARK: - File Cache Errors enum
 enum FileCacheErrors: Error {
     case noSuchFileOrDirectory
     case upparsableData
+    case databaseConnetionError
 }
 
 // MARK: - File Cache class
@@ -89,5 +91,82 @@ extension FileCache {
         }
         let data = Data(dataString.utf8)
         try data.write(to: path)
+    }
+}
+
+extension FileCache {
+    
+    /// Save Todo Items to SQLite database
+    /// - Parameters:
+    ///   - todoItems: Array of TodoItem, which need to save
+    ///   - file: Name of sqlite file
+    /// - Throws: Errors that occur when saving data to the database
+    static func saveSQLite(todoItems: [TodoItem], to file: String) throws {
+        
+        let db = try Connection(file)
+        
+        let table = Table("todo_items")
+        
+        let id = Expression<String>("id")
+        let text = Expression<String>("text")
+        let priority = Expression<String>("priority")
+        let deadline = Expression<Date?>("deadline")
+        let isCompleted = Expression<Bool>("isCompleted")
+        let dateOfCreation = Expression<Date>("dateOfCreation")
+        let dateOfChange = Expression<Date?>("dateOfChange")
+        
+        try db.run(table.create(ifNotExists: true) { t in
+            t.column(id, unique: true)
+            t.column(text)
+            t.column(priority)
+            t.column(deadline)
+            t.column(isCompleted)
+            t.column(dateOfCreation)
+            t.column(dateOfChange)
+        })
+        
+        try db.run(table.delete())
+        
+        for item in todoItems {
+            let insert = table.insert(id <- item.id, text <- item.text, priority <- item.priority.rawValue, deadline <- item.deadline, isCompleted <- item.isCompleted, dateOfCreation <- item.dateOfCreation, dateOfChange <- item.dateOfChange)
+            
+            try db.run(insert)
+        }
+        
+    }
+    
+    /// Load data from SQLite database and return array of TodoItem
+    /// - Parameter file: Name of SQLite database file
+    /// - Returns: Array of TodoItem
+    /// - Throws: Errors that occur when loading data from the database
+    static func loadSQLite(from file: String) throws -> [TodoItem] {
+        guard let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            throw FileCacheErrors.noSuchFileOrDirectory
+        }
+        
+        let path = dir.appending(path: file)
+        
+        do {
+            let db = try Connection(path.absoluteString)
+            
+            let table = Table("todo_items")
+            
+            let id = Expression<String>("id")
+            let text = Expression<String>("text")
+            let priority = Expression<String>("priority")
+            let deadline = Expression<Date?>("deadline")
+            let isCompleted = Expression<Bool>("isCompleted")
+            let dateOfCreation = Expression<Date>("dateOfCreation")
+            let dateOfChange = Expression<Date?>("dateOfChange")
+            
+            var todoItems: [TodoItem] = []
+            for item in try db.prepare(table) {
+                todoItems.append(TodoItem(id: item[id], text: item[text], priority: Priority(rawValue: item[priority]) ?? .normal, deadline: item[deadline], isCompleted: item[isCompleted], dateOfCreation: item[dateOfCreation], dateOfChange: item[dateOfChange]))
+            }
+            
+            return todoItems
+        } catch {
+            throw error
+        }
     }
 }
