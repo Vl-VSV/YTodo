b832ea4b910b00bc2cd3b24b1140534b1634ce7c
Vl-VSV <vvs_2015@icloud.com>
2023-07-16 12:44:51 +0300

    Add target YTodoSwiftUI

---
+++ YTodoSwiftUI/YTodoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTodoSwiftUIApp.swift
+//  YTodoSwiftUI
+//
+//  Created by Vlad V on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct YTodoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
